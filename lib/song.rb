06798885c3484c4cbd89299e5ab1a39153d88c5d
Nash Brancic <nashfft@gmail.com>
2019-03-28 00:31:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,8 @@ class Song
   
   def self.new_by_filename(filename)
     song = filename.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
+    song = Song.new(song[1])
+    song.artist_name = song[0]
     song
   end
 
