2a4f249f2a9bf4d6c72f9e7462b248ec691e447b
Nash Brancic <nashfft@gmail.com>
2019-03-28 00:33:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,39 +1,39 @@
-require 'pry'
+# require 'pry'
 
-class Song
+# class Song
   
-  attr_accessor :artist, :name
+#   attr_accessor :artist, :name
 
-  def initialize(name)
-    @name = name
-  end
-  # def self.new_by_filename(filename)
-  #   song = filename.split(" - ")[1]
-  #   artist = filename.split(" - ")[0]
-  #   new_song = self.new(song)
-  #   new_song.artist_name = artist
-  #   new_song
-  # end 
+#   def initialize(name)
+#     @name = name
+#   end
+#   # def self.new_by_filename(filename)
+#   #   song = filename.split(" - ")[1]
+#   #   artist = filename.split(" - ")[0]
+#   #   new_song = self.new(song)
+#   #   new_song.artist_name = artist
+#   #   new_song
+#   # end 
   
-  # def self.new_by_filename(filename)
-  #   song = filename.chomp(".mp3").split(" - ")
-  #   song = Song.new(song[1])
-  #   song.artist_name = song[0]
-  #   song
-  # end
+#   # def self.new_by_filename(filename)
+#   #   song = filename.chomp(".mp3").split(" - ")
+#   #   song = Song.new(song[1])
+#   #   song.artist_name = song[0]
+#   #   song
+#   # end
   
-    def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
-    song
-  end
-end
+#     def self.new_by_filename(file)
+#     song_info = file.chomp(".mp3").split(" - ")
+#     song = Song.new(song_info[1])
+#     song.artist_name = song_info[0]
+#     song
+#   end
+# end
   
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
+#   def artist_name=(name)
+#     self.artist = Artist.find_or_create_by_name(name)
+#     artist.add_song(self)
+#   end
 
-end
+# end
 
