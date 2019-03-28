152cd101ecc6238e01c7185db94a52f80b8bc073
Nash Brancic <nashfft@gmail.com>
2019-03-28 00:38:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,60 +1,60 @@
-# require 'pry'
-
-class Song
-  attr_accessor :artist, :name
-
-  def initialize(name)
-    @name = name
-  end
-
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
-
-  def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
-    song
-  end
-end
-
+# # require 'pry'
 
 # class Song
-  
 #   attr_accessor :artist, :name
 
 #   def initialize(name)
 #     @name = name
 #   end
-#   # def self.new_by_filename(filename)
-#   #   song = filename.split(" - ")[1]
-#   #   artist = filename.split(" - ")[0]
-#   #   new_song = self.new(song)
-#   #   new_song.artist_name = artist
-#   #   new_song
-#   # end 
-  
-#   # def self.new_by_filename(filename)
-#   #   song = filename.chomp(".mp3").split(" - ")
-#   #   song = Song.new(song[1])
-#   #   song.artist_name = song[0]
-#   #   song
-#   # end
-  
-#     def self.new_by_filename(file)
+
+#   def artist_name=(name)
+#     self.artist = Artist.find_or_create_by_name(name)
+#     artist.add_song(self)
+#   end
+
+#   def self.new_by_filename(file)
 #     song_info = file.chomp(".mp3").split(" - ")
 #     song = Song.new(song_info[1])
 #     song.artist_name = song_info[0]
 #     song
 #   end
 # end
-  
-#   def artist_name=(name)
-#     self.artist = Artist.find_or_create_by_name(name)
-#     artist.add_song(self)
-#   end
 
-# end
+
+class Song
+  
+  attr_accessor :artist, :name
+
+  def initialize(name)
+    @name = name
+  end
+  # def self.new_by_filename(filename)
+  #   song = filename.split(" - ")[1]
+  #   artist = filename.split(" - ")[0]
+  #   new_song = self.new(song)
+  #   new_song.artist_name = artist
+  #   new_song
+  # end 
+  
+  # def self.new_by_filename(filename)
+  #   song = filename.chomp(".mp3").split(" - ")
+  #   song = Song.new(song[1])
+  #   song.artist_name = song[0]
+  #   song
+  # end
+  
+    def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
+end
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
+end
 
