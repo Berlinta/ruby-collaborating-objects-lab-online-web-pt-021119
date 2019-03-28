6fac1892ee63d2623cd2953aa3ab05b266383ee0
Nash Brancic <nashfft@gmail.com>
2019-03-28 00:21:13 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Song
+  
+  attr_accessor :artist, :name
+
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.new_by_filename(filename)
+    song = filename.split(" - ")[1]
+    artist = filename.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+    binding.pry
+  end 
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
+end
