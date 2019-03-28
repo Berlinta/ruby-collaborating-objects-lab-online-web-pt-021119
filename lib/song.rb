2f5e0353dcf206499a97d65411f45190068fd5f1
Nash Brancic <nashfft@gmail.com>
2019-03-28 00:32:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,10 +21,10 @@ class Song
     song.artist_name = song[0]
     song
   end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 
 end
 
-  # def artist_name=(name)
-  #   self.artist = Artist.find_or_create_by_name(name)
-  #   artist.add_song(self)
-  # end
