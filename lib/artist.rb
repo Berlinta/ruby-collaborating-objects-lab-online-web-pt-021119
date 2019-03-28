c4c70147692113962dcc43454aeae81faa7a13dc
Nash Brancic <nashfft@gmail.com>
2019-03-28 00:34:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,39 +1,39 @@
-class Artist
+# class Artist
   
-  attr_accessor :name 
-  attr_reader :songs
+#   attr_accessor :name 
+#   attr_reader :songs
   
-  @@all = []
+#   @@all = []
   
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#   end
   
-  def add_song(song)
-    @songs << song
-  end
+#   def add_song(song)
+#     @songs << song
+#   end
   
-  def save
-    @@all << self
-  end
+#   def save
+#     @@all << self
+#   end
   
-  def self.all
-    @@all
-  end
+#   def self.all
+#     @@all
+#   end
   
-  def self.find_or_create_by_name(name)
-    self.all.detect { |singer| singer.name == name } || Artist.new(name)
-  end
+#   def self.find_or_create_by_name(name)
+#     self.all.detect { |singer| singer.name == name } || Artist.new(name)
+#   end
   
-  def self.create(name)
-    artist = Artist.new(name)
-    artist.save
-    artist
-  end
+#   def self.create(name)
+#     artist = Artist.new(name)
+#     artist.save
+#     artist
+#   end
   
-  def print_songs
-    @songs.each { |song| puts song.name }
-  end
+#   def print_songs
+#     @songs.each { |song| puts song.name }
+#   end
   
-end
+# end
